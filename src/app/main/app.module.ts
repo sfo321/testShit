fff7d2e04c31c694a903596b8df627215dac363a
sfotev <sfotev@codixfr.private>
2018-01-30 12:20:03 +0200

    start

---
+++ src/app/main/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
